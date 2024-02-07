316b22ffb964f87782b4b0bcfa7fbe0f350d3c25
ksik94 <ksik@puzzlesystems.co.kr>
2024-02-07 11:48:04 +0900

    w_bank_account_widet.dart 20 line Gesturedetection -> Tap

--- lib/screen/main/tab/home/w_bank_account_widget.dart
+++ lib/screen/main/tab/home/w_bank_account_widget.dart
@@ -17,7 +17,7 @@ class BankAccountWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    return Tap(
       onTap: () =>__print(),
       child: Row(
         children: [
